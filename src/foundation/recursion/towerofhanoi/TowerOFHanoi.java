aa753b1f428a046d2bc702eca8777c88f3951ec9
rohitsengar001 <rohitsengar001@gmail.com>
2022-07-06 00:02:22 +0530

    Recursion:TowerOFHanoi

---
+++ src/foundation/recursion/towerofhanoi/TowerOFHanoi.java
@@ -0,0 +1,21 @@
+package foundation.recursion.towerofhanoi;
+
+import java.util.Scanner;
+
+public class TowerOFHanoi {
+    public static void TOH(int n, char from_rod, char to_rod, char aux_rod) {
+        if (n == 1) {
+            System.out.println("Move disk 1 from rod " +  from_rod + " to rod " + to_rod);
+            return;
+        }
+        TOH(n - 1, from_rod, aux_rod, to_rod);
+        System.out.println("Move Disk " + n + " from_rod " + from_rod + " to_rod " + to_rod + " via aux_rod " + aux_rod);
+        TOH(n - 1, aux_rod, to_rod, from_rod);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        TOH(n, 'A', 'B', 'C');
+    }
+}
