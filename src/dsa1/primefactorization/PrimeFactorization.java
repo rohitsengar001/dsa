bc008f5257b34b5d0f8ddb7c77093624a256b98b
rohitsengar001 <rohitsengar001@gmail.com>
2022-07-12 23:18:16 +0530

    PrimeFactorization : Dsa 1

---
+++ src/dsa1/primefactorization/PrimeFactorization.java
@@ -0,0 +1,24 @@
+package dsa1.primefactorization;
+
+public class PrimeFactorization{
+    public int solution(int n){
+        int result = 1;
+        int i=2;
+        while(n > 1){
+            if(n % i != 0){
+                i++;
+            }else {
+                System.out.print(i);
+                result *=i;
+                n=n/i;
+            if(n>1) System.out.print( "* ");
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        PrimeFactorization pr = new PrimeFactorization();
+        System.out.print("= "+pr.solution(64));
+    }
+}
